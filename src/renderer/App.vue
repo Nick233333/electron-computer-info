bc37fc9cf22ae0542a5f93eb0f407cb57f41df43
nick <nick_php@163.com>
2019-03-11 18:44:39 +0800

    :tada: init

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'computer-info'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
---
+++ src/renderer/pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <hello-world></hello-world>
+</template>
+
+<script>
+import HelloWorld from '@/components/hello-world'
+
+export default {
+  components: {
+    HelloWorld
+  }
+}
+</script>
+

383e245b3daff9fe9af4e80e344caec8fe13611c
nick <nick_php@163.com>
2019-03-16 10:53:00 +0800

    :sparkles: add

--- src/renderer/router/index.js
+++ src/renderer/router/index.js
@@ -8,7 +8,7 @@ export default new Router({
     {
       path: '/',
       name: 'landing-page',
-      component: require('@/pages/index').default
+      component: require('@/views/index').default
     },
     {
       path: '*',

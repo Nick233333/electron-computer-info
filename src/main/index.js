bae6cac51812737db001f9bb91d11f54ddabb9b8
nick <nick_php@163.com>
2019-03-23 17:11:38 +0800

    :sparkles: update tary

--- src/main/index.js
+++ src/main/index.js
@@ -28,7 +28,7 @@ function createWindow () {
     show: true, // 创建后是否显示
     frame: true, // 是否创建frameless窗口
     fullscreenable: true, // 是否允许全屏
-    fullscreen: true,
+    fullscreen: false,
     center: true, // 是否出现在屏幕居中的位置
     backgroundColor: '#fff', // 背景色，用于transparent和frameless窗口
     titleBarStyle: '#f90', // 标题栏的样式，有hidden、hiddenInset、customButtonsOnHover等
@@ -76,7 +76,7 @@ function createContextMenu () {
 }
 
 function createTray () {
-  const menubarPic = process.platform === 'darwin' ? `${__static}/favicon.png` : `${__static}/favicon.png`
+  const menubarPic = process.platform === 'darwin' ? `${__static}/logo-darwin.png` : `${__static}/logo-win32.png`
   tray = new Tray(menubarPic)
   tray.on('right-click', () => {
     contextMenu = createContextMenu()
